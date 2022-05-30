28df3f91dd7103d035f1ff99b672ab71ecf85fc5
DESKTOP-6QMTRQS\GIAUEM <lethigiauem2809@gmail.com>
2022-05-30 07:39:10 +0700

    Ui quản lý vai trò

---
+++ EOffice.Web/src/pages/quanlytaikhoan/quyen/data.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: "1",
+    code: "demo",
+    fullName: "demo",
+    createDate: "25/02/2022",
+    updateDate: "28/02/2022",
+  },
+  {
+    id: "2",
+    code: "demo1",
+    fullName: "demo 1",
+    createDate: "25/02/2022",
+    updateDate: "28/02/2022",
+  },
+  {
+    id: "3",
+    code: "demo 2",
+    fullName: "demo 2",
+    createDate: "25/02/2022",
+    updateDate: "28/02/2022",
+  },
+];
---
+++ EOffice.Web/src/pages/quanlytaikhoan/quyen/themmoi.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class="">
+    <div class="mb-3">
+      <label for="validationCode" class="form-label">Code</label>
+      <span class="text-danger">*</span>
+      <input type="text" class="form-control" id="validationCode" name="name" />
+    </div>
+    <div class="mb-3">
+      <label for="validationName" class="form-label">Tên quyền</label>
+      <sapn class="text-danger">*</sapn>
+      <input type="text" class="form-control" id="validationName" name="name" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
